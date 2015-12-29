3391d11d392625bcfebd2f7c415cd95bf6f627d1
mephix <john.prins@gmail.com>
2015-12-28 19:43:09 -0800

    create fullyConditionalSpecification file
    create fullyConditionalSpecification file containing fcs, fcsOneColumn,
    and seed functions.

---
+++ fullyConditionalSpecification.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+
+def fcs(data,fitmethod):
+    """ fitmethod can be 'ols' or 'extra' (for extremely randomized forest) """
+
+    import multiprocessing as mp
+    from functools import partial
+
+    # data has already been seeded; it contains no NaNs
+    nFeatures = data.shape[1]
+
+    # start a pool of multiprocessors
+    # number of processes defaults to number of cores on your machine
+    pool = mp.Pool(processes=min(nFeatures, mp.cpu_count()))
+
+    # give the same dataset to each processor, but indexing a different column to forecast
+    pooledResults = pool.map(partial(fcsOneColumn, data, fitmethod), range(nFeatures))
+    forecastedDataList = [pooledResult[0] for pooledResult in pooledResults]
+    scores = [pooledResult[1] for pooledResult in pooledResults]
+
+    # do some cleanup (what does join do?)
+    pool.close()
+    pool.join()
+
+    # update the dataset's missing values with the forecasts
+    data_updated = np.copy(data)
+    for f in range(nFeatures):
+        nidx_f = np.isnan(data[:, f])
+        data_updated[nidx_f, f] = forecastedDataList[f][nidx_f]
+
+    return data_updated, scores
+
+
+def fcsOneColumn(data,fitmethod,yindex):
+    """forecast one variable using the others"""
+    # yindex is the column to forecast using the other columns
+
+    from sklearn import linear_model, cross_validation
+
+    # we can implement as many fit methods as we like
+    # next up: ExtraTreesRegressor
+    fitmethods = {'ols':linear_model.LinearRegression(fit_intercept=True, normalize=False, copy_X=True),
+                  'sgd':linear_model.SGDRegressor(fit_intercept=True)}
+
+    # X is every column except the column specified by yindex
+    ncols = data.shape[1]
+    X = data[:,[c for c in range(ncols) if c != yindex]]
+    y = data[:,yindex]
+
+    # Cross-Validation Score
+    scores = cross_validation.cross_val_score(fitmethods[fitmethod], X, y, cv=5)
+
+    # Prediction
+    fitmethods[fitmethod].fit(X,y)
+    yhat = fitmethods[fitmethod].predict(X)
+
+    # return the forecasts and the mean of the cross-validation scores
+    return yhat, scores.mean()
+
+
+def seed(data):
+    from scipy import stats
+
+    nFeatures = data.shape[1]
+    kdes = []
+    data_seeded = np.copy(data)
+    for i in range(nFeatures):
+        nidx_i = np.isnan(data[:,i])
+
+        # we need to get the sample distribution of each variable
+        kde_i = stats.gaussian_kde(data[~nidx_i,i])
+        kdes.append(kde_i)
+
+        # draw randomly from the sample distribution to fill in the missing values
+        sample_i = kdes[i].resample(sum(nidx_i)).T
+
+        # stupid reshaping due to broadcasting error
+        data_seeded[nidx_i,i] = sample_i.reshape(len(sample_i),)
+
+    return data_seeded
---
+++ simulateData.py
@@ -0,0 +1,47 @@
+def simulateData(nObs, nFeatures):
+    """ generate random data from multivariate normal distribution """
+    import numpy as np
+
+    # randomly generate correlation matrix for the distribution. All correlations must
+    # be between -1 and 1. 'random' uses the uniform distribution.
+    C = np.random.random((nFeatures, nFeatures)) * 2. - 1.
+    for i in range(nFeatures):
+        C[i, i] = 1.
+    # ensure symmetry
+    C = (C + C.T) / 2.
+    # randomly generate volatilities. All volatilities must be positive.
+    v = np.random.rand(nFeatures, 1) * 10.
+    # covariance matrix. Convolve volatilites and correlations. (v*v').*(C)
+    V = np.dot(v, v.T)
+    # alternative way:
+    V = np.outer(v, v)
+    cov = V * C
+    # randomly generate nFeatures
+    mu = np.random.rand(nFeatures, ) * 2. - 1.
+    # randomly generate data
+    simulated_data = np.random.multivariate_normal(mu, cov, nObs)
+    return simulated_data
+
+
+def randomlyNanOutSubsetOfData(data, frac):
+    """
+    Makes a copy of a dataset with a random fraction frac converted to NaNs
+    :param data: dataset
+    :param frac: Fraction of the data you want to be NaN (eg 0.29)
+    :return: copy of the data (ndarray) with a random fraction NaN'd out
+    """
+    import random
+    import math
+    import numpy as np
+
+    # make copy of the data
+    datan = np.copy(data)
+    (nObs, nFeatures) = datan.shape
+
+    # create a random index for the subsample that will be created to NaNs
+    random_index = sorted(random.sample(range(datan.size), int(math.floor(datan.size * frac))))
+    datan.shape = (nObs * nFeatures,)
+    datan[random_index] = np.nan
+    datan.shape = (nObs, nFeatures)
+
+    return datan
